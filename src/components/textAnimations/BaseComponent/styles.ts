81ceb632a2c7633672530161dd27f87ac1f90c34
Fabio Furtado <fabioassoni06@gmail.com>
2022-08-04 16:48:20 -0300

    block implemented waiting for improvements

--- src/components/textAnimations/BaseComponent/styles.ts
+++ src/components/textAnimations/BaseComponent/styles.ts
@@ -43,4 +43,39 @@ export const Char = styled.span<ICharProps>`
     `}
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    overflow: hidden;
+    position: relative;
+    width:auto;
+    & * {
+        display: inline-block;
+    }
+    @keyframes block {
+                0%{ 
+                    transform: translate(-110%, 0);
+                }
+                45%, 55%{
+                    transform: translate(0, 0);
+                }
+                100%{
+                    transform: translate(110%, 0);
+                }
+            }
+
+            &::after {
+                color: #000;
+                visibility: visible;
+                clip-path: inset(0 0 0 0);
+                content: '';
+                height: 100%;
+                width: 100%;
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                background: #000;
+                animation: block .5s .25s both;
+
+            }
+`;
---
+++ src/stories/Text/Block.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Block } from "../../index.js";
+
+import './styles.css';
+
+export default {
+  title: "Example/TextAnimations/Block",
+  component: Block,
+};
+
+const Template = (args) => {
+  return (
+      <div style={{display: 'flex', justifyContent: 'center'}}><Block text="Block" className="test" /></div> 
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
